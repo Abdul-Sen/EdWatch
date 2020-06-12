f505b55acee8e54d03333ffb6a92a76065779063
Abdul <abdul.rm.jahangir@gmail.com>
2020-06-12 15:11:51 -0500

    message input component created

---
+++ src/components/input/input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import genericImg from './generic.png';
+
+const UserImage = styled.img`
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+`;
+
+const InputBox = styled.input`
+    margin-left: 0.7rem;
+    width: 100%;
+    background-color: transparent;
+    border: none;
+`;
+
+const Container = styled.div`
+    display: flex;
+    border-radius: 3px;
+    padding: 1rem;
+    width: 100%;
+    background-color: ${props => props.theme.colors.backgroundColors.third};
+    margin: 1rem;
+`;
+
+function Input(props) {
+    return (
+        <Container>
+            <UserImage src={genericImg} />
+            <InputBox></InputBox>
+        </Container>
+    );
+}
+
+export default Input;
