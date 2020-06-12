101786f66c094760c517d037b018e710dc618375
Abdul <abdul.rm.jahangir@gmail.com>
2020-06-12 15:13:20 -0500

    comment box created

--- src/components/comment/comment.js
+++ src/components/comment/comment.js
@@ -1,17 +1,40 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import Input from '../input';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
 
 const Container = styled.div`
-    background-color: grey;
-    padding: 1rem;
+    background-color: transparent;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 90%;
+    padding: 0.5rem 1rem 0.5rem 1rem;
 `;
 
+const InputButton = styled.button`
+    background-color: ${props => props.theme.colors.iconColor.primary};
+    border-radius: 50%;
+    padding: 0.8rem;
+    border: none;
+    outline: none;
+    &:active {
+        background-color: ${props => props.theme.colors.iconColor.activePrimary};
+    }
+`;
+
+// This function adds a new comment to the chat
 function Comment(props)
 {
     return (
         <Fragment>
             <Container>
-                <p>Comment</p>
+                <Input />
+                <InputButton >
+                    <FontAwesomeIcon icon={faPaperPlane} />
+                </InputButton>
             </Container>
         </Fragment>
     )
