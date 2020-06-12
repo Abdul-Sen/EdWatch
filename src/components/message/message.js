338b458e4b4c9ba1ae9eee2e75c0e7a68e17000b
Abdul <abdul.rm.jahangir@gmail.com>
2020-06-12 14:52:18 -0500

    message component created

---
+++ src/components/message/message.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display:flex;
+    margin-bottom: 1rem;
+    background-color: ${props => props.theme.colors.backgroundColors.third};
+    border-radius: 10px;
+`;
+
+const ProfileImage = styled.img`
+    height: 3rem;
+    width: 3rem;
+    border-radius: 50%;
+`;
+const RightContainer = styled.div`
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+`;
+
+const Name = styled.p`
+    font-family: ${props => props.theme.fonts[0]};
+    font-size: 0.8rem;
+    margin-top: 5px;
+`;
+
+const ChatMessage = styled.p`
+    font-family: ${props => props.theme.fonts[1]};
+    margin-top: 8px;
+    margin-bottom: 8px;
+`;
+
+function Message({name, message,image})
+{
+    return(
+            <Container>
+                <ProfileImage src={image} />
+                <RightContainer>
+                    <Name>{name}</Name>
+                    <ChatMessage>{message}</ChatMessage>
+                </RightContainer>
+            </Container>
+    )
+}
+export default Message;
