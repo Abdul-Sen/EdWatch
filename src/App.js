bfbc643ea7352cbb156bb17a852a1cda9c0c3391
Abdul <abdul.rm.jahangir@gmail.com>
2020-06-13 02:46:01 -0500

    App layout defined

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Profile from "./components/profile";
 import Theme from "./components/theme";
 import styled from "styled-components";
 import ChatBox from './components/chatbox';
+import Navbar from './components/navbar';
+import Video from './components/video/';
 
 const Container = styled.div`
+  justify-content: space-around;
   width: 100%;
   min-height: 100vh;
   background-color: ${props => props.theme.colors.backgroundColors.primary};
@@ -18,15 +21,48 @@ const Container = styled.div`
   }
 `;
 
+const MainContainer = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100%;
+  min-height: 100vh;
+  background-color: lightblue;
+`;
+
+const LeftContainer = styled.div`
+width: 60%;
+@media (max-width: 768px) {
+      width: 90%;
+  }
+`;
+
+const RightContainer = styled.div`
+width: 30%;
+@media (max-width: 768px) {
+      width: 90%;
+  }
+`;
+
+const VideoStyled = styled(Video)`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
 export default function App() {
   return (
     <Theme>
-      <Container>
-        <Profile style={{width: "50%"}}  />
-        <div style={{width: "50%"}} >
-          <ChatBox />
-        </div>
-      </Container>
+      <MainContainer>
+        <Navbar></Navbar>
+        <Container>
+          <LeftContainer >  
+            <VideoStyled youtubeId="lTRiuFIWV54" />
+            <Profile />
+          </LeftContainer>
+          <RightContainer>
+              <ChatBox />
+          </RightContainer>
+        </Container>
+      </MainContainer>
     </Theme>
   );
-}
+} 
