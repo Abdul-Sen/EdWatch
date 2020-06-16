199ce55754c65383303244c7ce81b944c042ee62
Abdul <abdul.rm.jahangir@gmail.com>
2020-06-16 02:29:52 -0500

    chatbox minheight changed to fix scroll overflow. Redux added

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,33 +1,24 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 import Message from '../message';
+import {useSelector} from 'react-redux';
+
 const Container = styled.div`
     display: flex;
     flex-direction: column;
+    max-height: 80vh;
+    overflow: auto;
 `;
 
 // This component is responsible for displaying a history of chat messages
 function Chat (props)
 {
-    const [messages] = useState([
-        {
-            image : (process.env.PUBLIC_URL + '/avatars/Artboards_Diversity_Avatars_by_Netguru-1.png'),
-            name: 'drake',
-            message: 'This is the viral video i was talking about :)'
-        },
-        {
-            image : (process.env.PUBLIC_URL + '/avatars/Artboards_Diversity_Avatars_by_Netguru-2.png'),
-            name: 'Random Name',
-            message: 'oh this one'
-        }
-    ]);
-
+    const messages = useSelector(gstate => gstate.messages);
     return(
             <Container>
                 {messages.map((val,index)=> {
                 return <Message name={val.name} message={val.message} image={val.image} key={index} />})}
             </Container>
-
     )
 }
 
