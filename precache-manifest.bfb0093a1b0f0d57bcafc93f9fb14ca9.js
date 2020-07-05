3679e9765a06a19dad6fd59fc9c1143990a8aa3e
Abdul <abdul.rm.jahangir@gmail.com>
2020-07-05 14:48:12 -0500

    Updates

---
+++ precache-manifest.bfb0093a1b0f0d57bcafc93f9fb14ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee23a42f65e2379a07a6dfdb9fcb4fae",
+    "url": "/EdWatch/index.html"
+  },
+  {
+    "revision": "bfb34c4ee5c6bab96034",
+    "url": "/EdWatch/static/css/main.d9bfba4f.chunk.css"
+  },
+  {
+    "revision": "fc6cf82e8e99302ff4f3",
+    "url": "/EdWatch/static/js/2.800407b3.chunk.js"
+  },
+  {
+    "revision": "ea2e8646759e44825f7c612bdaa72019",
+    "url": "/EdWatch/static/js/2.800407b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb34c4ee5c6bab96034",
+    "url": "/EdWatch/static/js/main.4222d26d.chunk.js"
+  },
+  {
+    "revision": "7d35e8f4ff2d8cd43a17",
+    "url": "/EdWatch/static/js/runtime-main.56c5c196.js"
+  }
+]);
