aaf4b4a58fc6bfaac9ea83f4fcda0d51f6ce9fad
Abdul <abdul.rm.jahangir@gmail.com>
2020-07-14 02:36:33 -0500

    copy and play support for search results

--- src/components/searchResult/searchResult.js
+++ src/components/searchResult/searchResult.js
@@ -1,5 +1,11 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import { getIsHost } from '../../utils/signalR';
+import { useDispatch } from 'react-redux';
+import { updateVideoStateAsync } from '../../actions/videoState';
+import {toast} from 'react-toastify';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle, faCheck} from "@fortawesome/free-solid-svg-icons";
 
 const Container = styled.div`
     display: grid;
@@ -9,7 +15,9 @@ const Container = styled.div`
     grid-template-rows: 100px 5px;
     grid-column-gap: 13px;
     grid-row-gap: 7px;
-    /* grid-gap: 40px; */
+    &:active {
+        background-color: ${props => props.theme.colors.iconColor.activePrimary};
+    }
 `;
 
 const ImageContainer = styled.img`
@@ -37,14 +45,48 @@ const SubHead = styled.h5`
     color: ${props => props.theme.colors.textColors.secondary};
     margin-bottom: ${props => props.theme.fontSizes.xsmall};
 `;
-function SearchResult({data})
-{
+function SearchResult({ data }) {
+
+    const dispatch = useDispatch();
+
+    const IconWrap = ({userIcon, children})=>  <Fragment><FontAwesomeIcon icon={userIcon}> </FontAwesomeIcon> {children}</Fragment>;
+
+    const handleVideoSelect = () => {
+        console.log(`video URL is... ${data.video_url}`);
+        if (getIsHost()) {
+            // Play selected video
+            console.log(`user is host, playing new video`);
+            dispatch(updateVideoStateAsync({url: data.video_url}));
+            toast(<IconWrap userIcon={faCheck}>Video loaded for all users</IconWrap>, {delay: 1000});
+        }
+        else {
+            console.log(`user is not host, copying video to clipboard`);
+            // Copy a message to your clipboard
+            let message = `Please play ${data.video_url}`;
+            // Create new element
+            let el = document.createElement('textarea');
+            // Set value (string to be copied)
+            el.value = message;
+            // Set non-editable to avoid focus and move outside of view
+            el.setAttribute('readonly', '');
+            el.style = { position: 'absolute', left: '-9999px' };
+            document.body.appendChild(el);
+            // Select text inside element
+            el.select();
+            // Copy text to clipboard
+            document.execCommand('copy');
+            // Remove temporary element
+            document.body.removeChild(el);
+            toast.info(<IconWrap userIcon={faInfoCircle}>Copied message to your clipboard, just paste in chat ✌️</IconWrap>);
+        }
+    }
+
     return <Fragment>
-        <Container >
+        <Container onClick={handleVideoSelect}>
         <ImageContainer src={data.thumbnail} />
         <ContentContainer>
             <H2>{data.title}</H2>
-<SubHead>{data.creator} * {data.uploadDate}</SubHead>
+<SubHead>{data.creator} • {data.uploadDate}</SubHead>
 <SubHead>{data.description}</SubHead>
         </ContentContainer>
         </Container>
