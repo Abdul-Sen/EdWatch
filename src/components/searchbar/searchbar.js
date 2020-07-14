133f8bf041ac66911144ed23ffadaa5bc9a8134e
Abdul <abdul.rm.jahangir@gmail.com>
2020-07-14 02:34:24 -0500

    Updated searchbar to better suite aesthetics

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -1,35 +1,81 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import useDebounce from '../../customHooks/useDebounce';
-import {searchForVideos} from '../../api/videoSearch';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 
 const InputSearch = styled.input`
-  
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 10px 10px 40px -10px rgba(0,0,0,0.75);
+  margin: 1rem;
+  outline: none;
 `;
 
-function SearchBar({onChangeHandler, apibaseHandler}) {
+const SearchBarInput = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 10fr;
+  justify-items: stretch;
 
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 5px 10px 15px -15px rgba(0,0,0,0.75);
+  margin: 1rem;
+  background-color: #ffffff;
+`;
+const MainBar = styled.div`
+@media (max-width: 768px) {
+  grid-template-columns: 1fr;
+}
+
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  justify-items: stretch;
+  justify-content: space-evenly;
+  margin-bottom: 50px;
+`;
+
+const CustomSelect = styled.select`
+align-self: center;
+    padding: 1rem;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 5px 5px 15px -15px rgba(0,0,0,0.75);
+    outline: none;
+    padding: 1rem;
+  margin: 1rem;
+`;
+
+const CustomInput = styled.input`
+  outline: none;
+  border:none;
+`;
+
+const Option = styled.option`
+${({ disabled }) => disabled && `
+    color: #cfcfcf;
+  `}
+`;
+
+function SearchBar({ onChangeHandler, apibaseHandler }) {
   return (
     <Fragment>
-      <InputSearch type="text" onChange={onChangeHandler} />
-      <select name="apiBase" id="apiBase" onChange={apibaseHandler}>
-        <option value="youtube">YouTube</option>
-        <option value="dailyMotion">Daily Motion</option>
-        <option value="vimeo">Vimeo</option>
-        <option value="twitch">Twitch</option>
-      </select> 
-      
-      {/* searchbar
-      <input type={"text"} onChange={e => setSearchTerm(e.target.value)}></input>
-      <label for="apiBase">Choose the site you want to search</label>
-      <select name="apiBase" id="apiBase" onChange={changeApiBase}>
-        <option value="youtube">YouTube</option>
-        <option value="dailyMotion">Daily Motion</option>
-        <option value="vimeo">Vimeo</option>
-        <option value="twitch">Twitch</option>
-      </select> */}
-
+    <MainBar>
+      <SearchBarInput>
+          <FontAwesomeIcon style={{alignSelf: "center", justifySelf: "center"}} icon={faSearch} />
+          <CustomInput placeholder={"Search Videos"} type="text" onChange={onChangeHandler} />
+      </SearchBarInput>
+        <CustomSelect name="apiBase" id="apiBase" onChange={apibaseHandler}>
+          <Option value="youtube">YouTube</Option>
+          <Option value="vimeo">Vimeo</Option>
+          <Option disabled value="twitch">Twitch</Option>
+          <Option disabled value="dailyMotion">Daily Motion</Option>
+        </CustomSelect>
+        </MainBar>
     </Fragment>
   )
 }
